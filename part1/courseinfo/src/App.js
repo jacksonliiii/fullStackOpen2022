ddde793320c22fd94caf65f57d45d9b35c15df16
jacksonliiii <jacksonli2001@hotmail.com>
2022-12-26 09:55:05 -0800

    Work on part1 exercises

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,55 @@
+
+const Header = (props) => {
+
+  return (
+    <div>
+      <h1>
+        {props.course}
+      </h1>
+    </div>
+  )
+}
+
+const Part = (props) => {
+
+  return (
+    <div>
+      <p>
+        {props.part} {props.exercises}
+      </p>
+    </div>
+  )
+}
+
+const Content = () => {
+
+  return (
+    <div>
+      <Part part={'Fundamentals of React'} exercises={10}/>
+      <Part part={'Using props to pass data'} exercises={7}/>
+      <Part part={'State of a component'} exercises={14}/>
+    </div>
+  )
+}
+
+const Total = () => {
+  return (
+    <div>
+      Number of exercises {31}
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content  />
+      <Total  />
+    </div>
+  )
+}
+
+export default App;
