57f645156e23ec470d13ad92673683643613606e
jacksonliiii <jacksonli2001@hotmail.com>
2023-01-03 15:53:54 -0800

    Begin part2

---
+++ part2/src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import Note from './components/Note'
+
+const Note = ({ note }) => {
+  return (
+    <li>{note.content}</li>
+  )
+}
+
+const App = ({ notes }) => {
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
