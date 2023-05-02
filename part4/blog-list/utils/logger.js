12c4e4cfbbff806cf9a417695c3a93ed0faf7acf
jacksonliiii <jacksonli2001@hotmail.com>
2023-05-02 11:34:02 -0700

    Complete exercises 4.1-4.2

---
+++ part4/blog-list/utils/logger.js
@@ -0,0 +1,12 @@
+/*
+*   Handle usage of console.log and console.error
+*/
+const info = (...params) => {
+    console.log(...params)
+}
+
+const error = (...params) => {
+    console.error(...params)
+}
+
+module.exports = { info, error }
