09e172467e58e3db212c995e800e5750abff25b4
jacksonliiii <jacksonli2001@hotmail.com>
2023-05-08 10:54:29 -0700

    Work on exercise 4.8

--- part4/blog-list/tests/blog_api.test.js
+++ part4/blog-list/tests/blog_api.test.js
@@ -1,8 +1,14 @@
-const mongoose = require('mongoose');
-const supertest = require('supertest');
+const mongoose = require('mongoose')
+const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
+const Blog = require('../models/blog')
+const BlogHelper = require('./test_helper')
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(BlogHelper.initialBlogs)
+})
 
 describe('Initial Tests', () => {
     test('Blogs are returned as JSON', async () => {
@@ -12,7 +18,13 @@ describe('Initial Tests', () => {
             .expect('Content-Type', /application\/json/)
     })
 
-    afterAll(async () => {
-        await mongoose.connection.close()
+    test('All blogs are returned', async () => {
+        const response = await api.get('/api/blogs')
+
+        expect(response.body).toHaveLength(BlogHelper.initialBlogs.length)
     })
+})
+
+afterAll(async () => {
+    await mongoose.connection.close()
 })
