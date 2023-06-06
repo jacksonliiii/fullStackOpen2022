72677e62d053c6318e32d432a08ea84ad20144c6
jacksonliiii <jacksonli2001@hotmail.com>
2023-06-06 15:22:41 -0700

    Complete exercises 9.8-9.9

---
+++ part9/patientor-backend/src/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = 3001;
+
+app.get("/api/ping", (_req, res) => {
+  console.log("someone pinged here");
+  res.send("pong");
+});
+
+app.get("/api/patients", (_req, res) => {
+  console.log("patients was pinged");
+  res.send("patients will go here");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
